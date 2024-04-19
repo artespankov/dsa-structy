312a48bead9832ec6463dc73fe4ae66066767350
Artem Pankov <ae_apankov@idcontractors.net>
2024-04-19 13:26:16 +0200

    dp - refactored trib

--- dynamic/tribonacci.py
+++ dynamic/tribonacci.py
@@ -1,21 +1,11 @@
-m = {}
-def tribonacci(n):
-    if n == 1 or n == 0:
-        return 0
-    if n == 2:
-        return 1
-    v1 = m[n-1] if n-1 in m else tribonacci(n-1)
-    m[n-1] = v1
-    v2 = m[n-2] if n-2 in m else tribonacci(n-2)
-    m[n-2] = v2
-    v3 = m[n-3] if n-3 in m else tribonacci(n-3)
-    m[n-3] = v3
-    return v1 + v2 + v3
-
-
 from functools import lru_cache
+
 @lru_cache(maxsize=None)
-def tribonacci2(n):
+def tribonacci(n):
+    """
+    Time: O(n)
+    Space: O(n)
+    """
     if n == 1 or n == 0:
         return 0
     if n == 2:
@@ -23,12 +13,28 @@ def tribonacci2(n):
     return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
 
 
+m = {}
+def tribonacci_rec_memoization(n):
+    """
+    Time: O(3^n)
+    Space: O(n)
+    """
+    if n in m:
+        return m[n]
+    if n == 1 or n == 0:
+        return 0
+    if n == 2:
+        return 1
+    m[n] = tribonacci_rec_memoization(n-1) + tribonacci_rec_memoization(n-2) + tribonacci_rec_memoization(n-3)
+    return m[n]
+
+
 if __name__ == "__main__":
-    tribonacci(0)  # -> 0
-    tribonacci(1)  # -> 0
-    tribonacci(2)  # -> 1
-    tribonacci(5)  # -> 4
-    tribonacci(7)  # -> 13
-    tribonacci(14)  # -> 927
-    tribonacci(20)  # -> 35890
-    tribonacci(37)  # -> 1132436852
+    tribonacci_rec_memoization(0)  # -> 0
+    tribonacci_rec_memoization(1)  # -> 0
+    tribonacci_rec_memoization(2)  # -> 1
+    tribonacci_rec_memoization(5)  # -> 4
+    tribonacci_rec_memoization(7)  # -> 13
+    tribonacci_rec_memoization(14)  # -> 927
+    tribonacci_rec_memoization(20)  # -> 35890
+    tribonacci_rec_memoization(37)  # -> 1132436852
